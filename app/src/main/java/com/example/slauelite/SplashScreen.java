e0eae9f928e23af582bf4b1f046e732b3d9ac0d8
Balabyeki Jonathan <jonathanesh50@gmail.com>
2021-11-13 09:06:40 +0300

    splash screen working fine now

--- app/src/main/java/com/example/slauelite/SplashScreen.java
+++ app/src/main/java/com/example/slauelite/SplashScreen.java
@@ -5,11 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.transition.Explode;
-import android.view.Window;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -18,42 +15,41 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-            getWindow().setEnterTransition(new Explode());
-        }
+
         if (!splashLoaded){
             setContentView(R.layout.activity_splash_screen);
 
-            final int secondsDelayed = 5;
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    MyDatabaseHandler dbHandler = new MyDatabaseHandler(getApplicationContext());
-                    SQLiteDatabase db = dbHandler.getReadableDatabase();
-                    UserContract uc = new UserContract();
 
-                    Cursor cursor = db.query(uc.getTableName(), null, null, null, null, null, null);
-
-                    if (cursor.getCount() > 0) {
-                        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }else {
-                        Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
+            final int secondsDelayed = 4;
+            new Handler().postDelayed(() -> {
+                startActivity(setRoute());
+                finish();
             }, secondsDelayed * 1000);
 
             splashLoaded = true;
         } else {
-            Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            startActivity(intent);
+            startActivity(setRoute());
             finish();
         }
+    }
 
+    public Intent setRoute() {
+        Intent intent;
+
+        MyDatabaseHandler dbHandler = new MyDatabaseHandler(getApplicationContext());
+        SQLiteDatabase db = dbHandler.getReadableDatabase();
+        UserContract uc = new UserContract();
+
+        Cursor cursor = db.query(uc.getTableName(), null, null, null, null, null, null);
+
+        if (cursor.getCount() > 0) {
+            intent = new Intent(SplashScreen.this, MainActivity.class);
+        }else {
+            intent = new Intent(SplashScreen.this, LoginActivity.class);
+        }
+        cursor.close();
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+
+        return intent;
     }
 }
